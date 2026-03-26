831fab6fe9504487eb9cd6d7bc763cc0629a5d07
Augusto <augusto.palves1@gmail.com>
2026-03-26 19:38:30 -0300

    Projeto crud_php - Listar & Excluir - ok

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$dbname = 'crud_php';
+$host = 'localhost:4306';
+$dbuser = 'root';
+$dbpass = '';
+
+
+try {
+    $db = new PDO("mysql:dbname=" . $dbname . ";host=" . $host. ";port=4306", $dbuser, $dbpass);
+} catch (PDOException $e) {
+    echo "Erro" . $e->getMessage();
+    exit();
+}
+define('BASE_URL', 'http://localhost/crud_php/');
+?>
